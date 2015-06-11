779cc5d145c5493b4efacc9b272ad9378161bff6
MrSquid <commandersquid1@gmail.com>
2015-06-12 07:55:31 +1000

    Added Leaderboard View and Controller

---
+++ Tappity/LeaderBoardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LeaderBoardViewController.swift
+//  Tappity
+//
+//  Created by Jon on 11/06/2015.
+//  Copyright (c) 2015 Arczo. All rights reserved.
+//
+
+import UIKit
+
+class LeaderBoardViewController: UITableViewController {
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    
+}
